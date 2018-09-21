32a7dbcec40ee1def1fe90ca58ce2f6279074727
SofieKalinke <sofie@Sofies-MacBook-Air-2.local>
2018-09-21 16:57:12 +0200

    Challenge 6 - Version 1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log('app is alive')
+
+function switchChannel(channelName){
+    console.log('Tuning into channel' + channelName);
+    document.getElementById('channelName').innerHTML=channelName;
+    //document.getElementById('location').innerHTML='by '+ <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>;
+    //document.getElementById('location').innerHTML='by upgrading.never.helps';
+
+    document.getElementById('location').innerHTML = 'upgrading.never.helps';
+    document.getElementById('location').href = "http://w3w.co/upgrading.never.helps";
+    document.getElementById('location').target = "_blank";
+}
+
+function switchStar(){
+    $('#starred').attr('src',"http://ip.lfe.mw.tum.de/sections/star-o.png");
+}
+
+function swithBackStar(){
+    $('#starred').attr('src',"http://ip.lfe.mw.tum.de/sections/star.png");
+}
+
+function selectTab(){}
